4a60b244d8e44fa6199fb3a906f7c760250cd876
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-11 12:50:31 +0530

    service-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ServiceBoxComponent } from './service-box/service-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ServiceBoxComponent
   ],// specifies or includes all the components.pipes and directives which
   //are part of the module
   imports: [
