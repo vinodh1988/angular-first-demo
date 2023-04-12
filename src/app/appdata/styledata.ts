92888ac83de0dfeb5657a76c6bb21fa1030b7387
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-12 10:33:23 +0530

    styling-ts

---
+++ src/app/appdata/styledata.ts
@@ -0,0 +1,23 @@
+export const theme1:any =  {
+    h1: {
+    borderBottom: "2px solid black",
+    paddingBottom: "20px",
+    color: "rgb(23, 23, 47)",
+    backgroundColor: "rgb(243, 217, 50)",
+    margin: "0px"
+},
+services:{
+    backgroundColor: "lavender",
+    height: "380px",
+    boxSizing: "border-box",
+    padding: "20px"
+}
+,footer: {
+    paddingBottom: "10px",
+    color: "rgb(23, 23, 47)",
+    backgroundColor: "rgb(243, 217, 50)",
+    borderTop: "2px solid black",
+    fontFamily: "Arial, Helvetica, sans-serif",
+    textAlign: "center"
+}
+}
